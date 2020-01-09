7b596cf44931870d6815245dd694f7a3beb3e8bb
mannymang <zijun@hotmail.co.jp>
2020-01-08 19:24:39 -0800

    Small bug fix
    Changed code to allow for midi files from different directories

--- pedalfix.py
+++ pedalfix.py
@@ -1,3 +1,5 @@
+#swaps every pedal press event with the note event occurring immediatley after in the track, fixing the pedal problems in VSTs like Kontakt Pianos and Pianoteq
+
 from mido import MidiFile
 import sys
 
@@ -9,7 +11,12 @@ debug = False
 
 print("processing...")
 
-save_str = "pf_" + sys.argv[1]    
+save_str = sys.argv[1]
+index = save_str.rindex("\\") + 1
+if index > 0:
+    save_str = save_str[:index] + "pf_" + save_str[index:]
+else:
+    save_str = "pf_" + sys.argv[1]
 
 mid = MidiFile(sys.argv[1])
 
@@ -20,9 +27,9 @@ for i, track in enumerate(mid.tracks):
     index = 0
     for msg in track:
         b = msg.bytes()
-	# if bytes in msg indicate pedal (64) and pressed (127)
+		# if bytes in msg indicate pedal (64) and pressed (127)
         if b[1] is 64 and b[2] is 127 and skip_toggle is False: 
-            # swap with the next msg in midi file
+            # swap with the next msg in midi file, meaning note
             next_msg = track[index + 1] # get next message
             prev_msg = msg
             msg = next_msg # set current slot to next msg
